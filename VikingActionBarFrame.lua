7a298b807feb44079463c117d81145a03d0bd6f4
Crowdedlight <crowdedhd@gmail.com>
2014-06-18 21:25:35 +0200

    removed requirement for chatlibary

--- VikingActionBarFrame.lua
+++ VikingActionBarFrame.lua
@@ -14,7 +14,7 @@ require "AbilityBook"
 require "ActionSetLib"
 require "AttributeMilestonesLib"
 require "Tooltip"
---require "ChatSystemLib"
+
 
 local VikingActionBarFrame = {}
 local VikingTooltipCursor = false
